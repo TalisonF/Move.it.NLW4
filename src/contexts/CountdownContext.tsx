ab11a3b07048b51fba6490d2129e50010274df59
Talisonf <talison737@gmail.com>
2021-02-26 20:29:59 -0300

    set time

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -37,7 +37,7 @@ export function CountdownProvider({ children }: ChallengeProviderProps) {
     function resetCountDown() {
         clearTimeout(countdownTimeout);
         setIsActive(false)
-        setTime(25 * 60)
+        setTime(0.1 * 60)
         setHasFinished(false)
     }
 
